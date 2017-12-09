09860c8c8ecfa51021b3106a565a12ce6ad7f82a
carloscubas <carlos.cubas@gmail.com>
2017-12-09 12:14:44 -0200

    inclusão de controle de acesso básico
    inclusão de uma acesso básico na aplicação

---
+++ src/main/java/com/example/demo/controladores/UserController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controladores;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.beans.Livro;
+
+@Controller
+public class UserController {
+	
+	@GetMapping("/login")
+	public ModelAndView login(@ModelAttribute Livro livro) {
+		
+		return new ModelAndView("login");
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpServletRequest request) {
+			
+		HttpSession session= request.getSession(false);
+	        	SecurityContextHolder.clearContext();
+	       	if(session != null) {
+	           		session.invalidate();
+	        	}
+
+		return "redirect:login";
+	}
+
+	
+	
+}
